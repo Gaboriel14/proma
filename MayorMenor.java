ddcabd6a48ebe57cd3daa9ce18115c64298529a4
Gabriel Emmanuel <gabrielemmanuelsotomacias@gmail.com>
2024-07-25 23:47:20 -0600

    mayor o menor

---
+++ MayorMenor.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication14;
+import java.util.Scanner;
+
+public class MayorMenor {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] numeros = new int[8];
+        int mayor, menor;
+
+        // Leer 8 números desde el teclado y almacenarlos en el array
+        for (int i = 0; i < 8; i++) {
+            System.out.print("Ingrese el número " + (i + 1) + ": ");
+            numeros[i] = scanner.nextInt();
+        }
+
+        // Inicializar mayor y menor con el primer elemento del array
+        mayor = numeros[0];
+        menor = numeros[0];
+
+        // Determinar el número mayor y menor
+        for (int i = 1; i < 8; i++) {
+            if (numeros[i] > mayor) {
+                mayor = numeros[i];
+            }
+            if (numeros[i] < menor) {
+                menor = numeros[i];
+            }
+        }
+
+        // Mostrar el número mayor y menor en la consola
+        System.out.println("El número mayor es: " + mayor);
+        System.out.println("El número menor es: " + menor);
+
+        scanner.close();
+    }
+}
+
+/**
+ *
+ * @author compu
+ */
+public class JavaApplication14 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
